330b895d6de565a9bcbffe841f94c7f97ba32278
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2021-12-29 02:23:32 -0800

    another test of code

--- Interaction_Observer/intersection_obs.js
+++ Interaction_Observer/intersection_obs.js
@@ -1,3 +1,8 @@
 const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show")
+    })
     console.log(entries)
 })
+
+observer.observe(cards[0])
