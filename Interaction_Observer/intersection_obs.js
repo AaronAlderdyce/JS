1e39b54f2aa0e7dcb092b3f410c546eca55a8c69
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2021-12-29 02:35:41 -0800

    refactored, retesting

--- Interaction_Observer/intersection_obs.js
+++ Interaction_Observer/intersection_obs.js
@@ -4,13 +4,11 @@ const cards = document.querySelectorAll(".card")
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("show", entry.isIntersecting)
+        if (entry.isIntersecting) observer.unobserve(entry.target)
     })
 }, {
     threshold: 1
 })
-
-
-
 
 cards.forEach(card => {
     observer.observe(cards)
