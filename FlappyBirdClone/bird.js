59da3aaaf00d3e2dc334d14b0686791369e53a86
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-01-05 17:46:56 -0800

    almost finished, first test

--- FlappyBirdClone/bird.js
+++ FlappyBirdClone/bird.js
@@ -3,13 +3,11 @@ const BIRD_SPEED = 0.5
 const JUMP_DURATION = 125
 let timeSinceLastJump = Number.POSITIVE_INFINITY
 
-
 export function setupBird() {
     setTop(window.innerHeight / 2)
     document.removeEventListener("keydown", handleJump)
     document.addEventListener("keydown", handleJump)
 }
-
 
 export function updateBird(delta) {
     if (timeSinceLastJump < JUMP_DURATION) {
@@ -19,24 +17,19 @@ export function updateBird(delta) {
     }
 
     timeSinceLastJump += delta
-
 }
-
 
 export function getBirdRect() {
     return birdElem.getBoundingClientRect()
 }
 
 function setTop(top) {
-    birdElem.getElementsByClassName.setProperty("--bird-top", top)
-
+    birdElem.style.setProperty("--bird-top", top)
 }
 
 function getTop() {
     return parseFloat(getComputedStyle(birdElem).getPropertyValue("--bird-top"))
-
 }
-
 
 function handleJump(e) {
     if (e.code !== "Space") return
