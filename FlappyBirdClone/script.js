48d449a8ac9fa6aaa10b5657c662369e045fbbd3
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-01-05 17:00:08 -0800

    working on javascript functionality

---
+++ FlappyBirdClone/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("keypress", handleStart, { once: true })
+const title = document.querySelector("[data-title]")
+let lastTime
+
+
+
+function updateLoop(time) {
+    console.log(time - lastTime)
+    lastTime = time
+    window.requestAnimationFrame(updateLoop)
+}
+
+function handleStart() {
+    title.classList.add("hide")
+    window.requestAnimationFrame(updateLoop)
+}
+
+function handlelose() {
+
+}
